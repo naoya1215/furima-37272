04239a4820f24c3bc2556b4b5e4dd36ab12b685e
naoya1215 <llobomit@icloud.com>
2022-03-27 19:45:11 +0900

    新規登録/ユーザー情報

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # パスワードは、半角英数字混合での入力が必須であること(以下の２行)。
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+end
