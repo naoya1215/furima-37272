d3e64cd7065200f01a8a7d0c3a62498c275fb4b9
naoya1215 <llobomit@icloud.com>
2022-04-11 14:40:26 +0900

    不要部分を削除
    console.logの削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,7 +3,6 @@ window.addEventListener('load', ()=>{
 const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     // 販売手数料(10%)
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue / 10);
