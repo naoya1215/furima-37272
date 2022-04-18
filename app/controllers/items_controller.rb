ecd276341effeb88e7474798d5ba501278914fc5
naoya1215 <llobomit@icloud.com>
2022-04-18 11:27:51 +0900

    リファクタリング
    メンターアドバイスにより、修正
    @item = Item.find(params[:id])
    コントローラ内に同じ@itemの定義が複数ありますので、privateメソッドにまとめてbefore_actionで呼び出しましょう。
    DRY原則に則ったリファクタリングのためです。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   # exceptを使用(index, showは除きユーザーがログインしているかを確認する)
   # ログインをしていない場合は、ログインページに遷移される
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update]
 
   def index # トップ画面の表示
     @items = Item.all.order(created_at: :desc)
@@ -22,12 +23,12 @@ class ItemsController < ApplicationController
 
   def show # 詳細ページへ
     # index.html.erbからクリックされた情報をパラメータとして受け取り、show.html.erbで詳細情報を表示
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id]) #リファクタリング
   end
 
   def edit #編集ページへ
     # new.html.erbからクリックされた情報をパラメータとして受け取り、edit.html.erbで編集ページを表示
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id]) #リファクタリング
     # 商品出品者以外が編集画面にアクセスしようとするとトップページに遷移される
     unless current_user.id == @item.user_id
       redirect_to root_path 
@@ -36,7 +37,8 @@ class ItemsController < ApplicationController
 
   def update #更新処理
     # error_messages.html.erbでmodelオプションを使用しているため、@item(インスタンス変数)を使用する必要がある
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id]) #リファクタリング
+    
     # 変更があった場合の処理
     if @item.update(item_params)
       #redirect_to(ルーティング→コントローラ→ビュー)、情報が更新されているのでぐるっとしているイメージ？
@@ -53,6 +55,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :responsibility_id, :prefecture_id, :shipping_id,
                                  :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @item = Item.find(params[:id])
   end
 
 end
