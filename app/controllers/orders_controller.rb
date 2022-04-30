d17c78970362544312d5007be6a60e2495ca4df8
naoya1215 <llobomit@icloud.com>
2022-04-30 11:42:35 +0900

    before_actionの使用
    メンターからのアドバイス
    理由は、同じコードを同ファイルで繰り返し書かないようにするためです。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_prototype, only: [:index, :create]
   def index
-    # 商品情報を表示するためのインスタンス
-    @item = Item.find(params[:item_id])
     # 自らの出品商品を購入できない
     if (current_user.id == @item.user_id) || (@item.order.present?)
       redirect_to root_path 
@@ -12,12 +11,10 @@ class OrdersController < ApplicationController
   end
   
   def create
-    @item = Item.find(params[:item_id])
     # 値をDBへ保存する実装
     @order_destination = OrderDestination.new(order_params)
     if @order_destination.valid?
       # 決算処理
-      #Payjp.api_key = "sk_test_9d65f712acf1933eb8751d24"  #直接秘密鍵を設定
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"] #環境変数を呼び込むようにした為、15行目の記述は削除
       Payjp::Charge.create(
         amount: @item.price,  # 商品の値段
@@ -35,4 +32,9 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_destination).permit(:post_code, :prefecture_id, :city, :address, :building, :phone).merge(user_id:current_user.id, item_id:@item.id, token: params[:token])
   end
+
+  def set_prototype
+    @item = Item.find(params[:id])
+  end
+
 end
